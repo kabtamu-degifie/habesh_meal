b3b09220b5933dfda6eb823b4d932ab5bf975c41
Kabtamu Degifie <kabtamu@etmsoftwareplc.com>
2023-05-11 07:09:52 +0300

    last update

--- lib/widgets/category_item.dart
+++ lib/widgets/category_item.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:habesh_meal/screens/category_meal_screen.dart';
 
 class CategoryItem extends StatelessWidget {
   final String title;
@@ -7,25 +6,17 @@ class CategoryItem extends StatelessWidget {
 
   const CategoryItem(this.title, this.color, {super.key});
 
-  void selectCategoryItem(BuildContext ctx) {
-    Navigator.of(ctx)
-        .push(MaterialPageRoute(builder: (_) => const CategoryMealScreen()));
-  }
-
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () => selectCategoryItem(context),
-      child: Container(
-        padding: const EdgeInsets.all(10),
-        decoration: BoxDecoration(
-            gradient: LinearGradient(
-                colors: [color.withOpacity(0.7), color],
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight),
-            borderRadius: BorderRadius.circular(15)),
-        child: Text(title),
-      ),
+    return Container(
+      padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+              colors: [color.withOpacity(0.7), color],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight),
+          borderRadius: BorderRadius.circular(15)),
+      child: Text(title),
     );
   }
 }
