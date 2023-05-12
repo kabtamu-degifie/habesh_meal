8b02660a8d65d0121d460e50acf97f2773f036b6
Kabtamu Degifie <kabtamu@etmsoftwareplc.com>
2023-05-12 14:52:42 +0300

    title update

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -9,7 +9,7 @@ class CategoriesScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Habesha Meal'),
+        title: const Text('Habesha Meal XX'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
